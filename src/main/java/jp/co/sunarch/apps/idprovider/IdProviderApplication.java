a793d32984923743b9140e803a44c0bd2f58e5f8
Takeshi Miyajima <rozyhead@gmail.com>
2017-07-31 00:13:36 +0900

    Initial commit

---
+++ src/main/java/jp/co/sunarch/apps/idprovider/IdProviderApplication.java
@@ -0,0 +1,12 @@
+package jp.co.sunarch.apps.idprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdProviderApplication.class, args);
+	}
+}
